b06008376c028e005d9f6f6db47455ec06904d80
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2023-07-02 15:09:57 +0100

    feat: add basic functionality with `stow`

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,28 @@
+
+alias vim nvim
+alias c clear
+alias ls lsd
+alias ip "ip -c"
+
+function notify-send
+  wsl-notify-send.exe --category $WSL_DISTRO_NAME "$argv"
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function reload-config
+  source ~/.config/fish/config.fish
+end
+
+starship init fish | source
+zoxide init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/hadronomy/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
