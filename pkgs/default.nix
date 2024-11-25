b0b6446f6d62638ba47cd8ac965e0eeffa10bf00
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-11-25 08:04:12 +0000

    chore(exp): add default derivation

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,1 +1,18 @@
-pkgs: {}
+{ pkgs, ... }:
+{
+  # NOTE: This is a simple derivation that echoes "Hello World!" when run.
+  # You can run it with `nix run .` or `nix-build . -A hello-world`.
+  # TODO: Autosetup script for the dotfiles
+  default = pkgs.stdenv.mkDerivation {
+    name = "hello-world-fzf";
+    src = null;
+    buildInputs = [ pkgs.fzf ];
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      echo '#!/bin/sh' > $out/bin/hello-world-fzf
+      echo 'echo "Hello World!" | ${pkgs.fzf}/bin/fzf' >> $out/bin/hello-world-fzf
+      chmod +x $out/bin/hello-world-fzf
+    '';
+  };
+}
