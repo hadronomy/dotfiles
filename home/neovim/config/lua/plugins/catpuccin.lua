3e0aa146a82366c596156820edb37ae22094c0b9
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-07-12 16:07:52 +0100

    feat: set nvim background to transparent

--- home/neovim/config/lua/plugins/catpuccin.lua
+++ home/neovim/config/lua/plugins/catpuccin.lua
@@ -4,6 +4,7 @@ return {
     lazy = true,
     name = "catppuccin",
     opts = {
+      transparent_background = true,
       integrations = {
         aerial = true,
         alpha = true,
@@ -46,6 +47,5 @@ return {
     opts = {
       colorscheme = "catppuccin",
     },
-  }
-
+  },
 }
