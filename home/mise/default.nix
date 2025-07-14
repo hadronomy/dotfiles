0aee0fb65b3b6ed1df295d8c12909a5d4defd491
Pablo Hernández <17086478+hadronomy@users.noreply.github.com>
2025-07-14 12:18:52 +0100

    feat: enable `idiomatic_version_file` in mise

--- home/mise/default.nix
+++ home/mise/default.nix
@@ -5,22 +5,29 @@
   programs = {
     hadronomy.mise = {
       enable = true;
-      globalConfig.tools = {
-        node = "lts";
-        bun = "latest";
-        deno = "latest";
-        go = "latest";
-        "cargo:cargo-nextest" = "latest";
-        "cargo:cargo-dist" = "latest";
-        "cargo:cargo-mutants" = "latest";
-        "cargo:cargo-binstall" = "latest";
-        "cargo:cargo-binutils" = "latest";
-        "cargo:cargo-edit" = "latest";
-        watchexec = "latest";
-        "npm:@antfu/ni" = "latest";
-        dotnet = "latest";
-        zig = "0.14.0";
-        zls = "0.14.0";
+      globalConfig = {
+        tools = {
+          node = "lts";
+          bun = "latest";
+          deno = "latest";
+          go = "latest";
+          "cargo:cargo-nextest" = "latest";
+          "cargo:cargo-dist" = "latest";
+          "cargo:cargo-mutants" = "latest";
+          "cargo:cargo-binstall" = "latest";
+          "cargo:cargo-binutils" = "latest";
+          "cargo:cargo-edit" = "latest";
+          watchexec = "latest";
+          "npm:@antfu/ni" = "latest";
+          dotnet = "latest";
+          zig = "0.14.0";
+          zls = "0.14.0";
+        };
+        settings = {
+          idiomatic_version_file_enable_tools = [
+            "node"
+          ];
+        };
       };
       settings.activate_aggresive = true;
     };
